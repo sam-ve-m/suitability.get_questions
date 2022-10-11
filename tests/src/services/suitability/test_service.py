ed927051de94dbf30409bae7b7fef35d6a5f0b64
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-10-11 09:39:54 -0300

    fix: service tests

--- tests/src/services/suitability/test_service.py
+++ tests/src/services/suitability/test_service.py
@@ -1,6 +1,6 @@
 # Jormungandr
 from func.src.services.suitability.service import SuitabilityService
-from tests.src.services.suitability.stubs import stub_suitability_model, stub_response
+from tests.src.services.suitability.stubs import stub_suitability_mongo_db
 
 # Standards
 from unittest.mock import patch
@@ -12,26 +12,26 @@ import pytest
 @pytest.mark.asyncio
 @patch(
     "func.src.services.suitability.service.SuitabilityRepository.get_latest_suitability",
-    return_value=stub_suitability_model,
+    return_value=stub_suitability_mongo_db,
 )
 @patch("func.src.services.suitability.service.AsyncCache.get", return_value=None)
 @patch(
     "func.src.services.suitability.service.config",
-    side_effect=["suitability_key", "suitability_key", "3600*24"],
+    side_effect=["suitability_key", "suitability_key", "86400"],
 )
 async def test_when_not_have_suitability_data_in_redis_then_get_in_mongo_and_set_in_redis_to_return_model(
     mock_decouple, mock_redis, mock_repository
 ):
-    suitability = await SuitabilityService.get_latest_questions_with_answers()
+    suitability = await SuitabilityService.get_latest_questions_with_answer_options()
 
     assert isinstance(suitability, dict)
     assert suitability.get("version") == 12
 
 
 @pytest.mark.asyncio
-@patch('func.src.services.suitability.service.AsyncCache.get', return_value=stub_response)
+@patch('func.src.services.suitability.service.AsyncCache.get', return_value=stub_suitability_mongo_db)
 async def test_when_have_suitability_in_redis_then_return_suitability_data(mock_redis):
-    suitability = await SuitabilityService.get_latest_questions_with_answers()
+    suitability = await SuitabilityService.get_latest_questions_with_answer_options()
 
     assert isinstance(suitability, dict)
     assert suitability.get("version") == 12
