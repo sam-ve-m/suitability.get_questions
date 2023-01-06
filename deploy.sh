6a5acab69dea7187efc81a08c5dfb56e7b88907e
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-06 13:35:51 -0300

    feat: versioning fission deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
 fission spec init
-fission env create --spec --name suitability-questions-env --image nexus.sigame.com.br/fission-async:0.1.7 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
-fission fn create --spec --name suitability-questions-fn --env suitability-questions-env --src "./func/*" --entrypoint main.get_suitability_questions --executortype newdeploy --maxscale 3
-fission route create --spec --name suitability-questions-rt --method GET --url /suitability/questions --function suitability-questions-fn
+fission env create --spec --name acc-suit-quest-get-env --image nexus.sigame.com.br/fission-account-suit-quest-get:0.1.0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name acc-suit-quest-get-fn --env acc-suit-quest-get-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
+fission route create --spec --name acc-suit-quest-get-rt --method GET --url /suitability/questions --function acc-suit-quest-get-fn
